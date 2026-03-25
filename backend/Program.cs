125307686bde19db04e9917527a515140c556a5e
Leandro Dailoff <leandrodailoff@outlook.com>
2026-03-24 23:50:37 -0300

    Fix port issue

--- backend/Program.cs
+++ backend/Program.cs
@@ -65,4 +65,5 @@ app.MapPut("/notes/{id}", async (int id, Note updated, AppDbContext db) =>
     return Results.Ok(note);
 });
 
-app.Run();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Run($"http://0.0.0.0:{port}");
