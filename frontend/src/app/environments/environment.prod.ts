26e7d8183b29a13af26e3129b49c8407f0d3bea1
Leandro Dailoff <leandrodailoff@outlook.com>
2026-03-28 00:13:27 -0300

    Configuracion de vercel y enviroment

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000' // replace with your production URL when ready
+  apiUrl: 'https://acordapp-bbezb6dzgdhufkc4.canadacentral-01.azurewebsites.net'
 };
